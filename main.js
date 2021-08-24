9677080ea0862cdfa2dab7b814a9cc43b2ab26a2
Motoki Tokifuji <motoki@tokifu.jp>
2021-08-24 15:53:24 +0900

    [modify] remove debug spreadsheet codes :beer:

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 function doPost(e) {
-  // スプレッドシートオブジェクトを取得
-  const sheet = SpreadsheetApp.getActive().getActiveSheet();
-
   // payload整形
   const bgnStr = 'payload={';
   const plString = replaceMention(decodeURIComponent(e.postData.contents));
   const pl = JSON.parse(plString.substring(bgnStr.length-1,plString.length));
   // slack投稿
   post2Slack(pl);
-
-  // 日付、postDataオブジェクトを追記
-  sheet.appendRow([new Date(), e.postData.contents]);
 }
 
 function replaceMention(text) {
